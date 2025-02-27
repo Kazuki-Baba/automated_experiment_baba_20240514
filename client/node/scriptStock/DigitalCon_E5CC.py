7822e19e9d4a65a0a5c312d52eab5569288c1602
Kazuki-Baba <149136227+Kazuki-Baba@users.noreply.github.com>
2025-02-27 15:29:35 +0900

    Update DigitalCon_E5CC.py

--- client/node/scriptStock/DigitalCon_E5CC.py
+++ client/node/scriptStock/DigitalCon_E5CC.py
@@ -1,29 +1,56 @@
-#!/usr/bin/python3
-
-# コマンドライン引数オプション
-# パッケージインポート
 import time
 import argparse
 import serial
 import modbus_tk.defines as cst
 from modbus_tk import modbus_rtu
+from modbus_tk.exceptions import ModbusError, ModbusInvalidResponseError
 
-# いつもの
+# コマンドライン引数の処理
 parser = argparse.ArgumentParser(description='コマンドライン引数で動作を分岐')
 
-parser.add_argument('--off'. help='恒温槽off', action='store_true')
+parser.add_argument('--off', help='恒温槽off(25℃へ戻す)', action='store_true')
 parser.add_argument('--get', help='温度をget', action='store_true')
 parser.add_argument('--set', help='温度をset', nargs=1)
-parser.add_argument(
-    '--sstate', help='waiting for stady state. set temp and maxTime', nargs=1)
+parser.add_argument('--sstate', help='waiting for stady state. set temp and maxTime', nargs=1)
 parser.add_argument('--setrun', help='温度をset&実行', nargs=1)
 parser.add_argument('--status', help='check the status', action='store_true')
 parser.add_argument('--setandsstate', help='温度をset & 定常待ち', nargs=1)
 
 args = parser.parse_args()
 
+# ModBus通信設定
+PORT = '/dev/ttyUSB0'
+e5cc_h = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=2))
+e5cc_h.set_timeout(20)
+Address = 1  # ユニット番号指定
 
-# ここから実行用コード
+# 書き込み許可(初期設定以外の時はコメントアウト推奨)
+# 動作指令の書き込み変数アドレスは0x0000
+# e5cc_h.execute(Address, cst.WRITE_SINGLE_REGISTER, 0x0000, output_value=0x0001)
+
+def commandInput(command, output):
+    # e5cc_h.execute(ユニットNo, 書き込み指定, 書き込む変数のアドレス, output_value=書き込む値（16進数）)
+    e5cc_h.execute(Address, cst.WRITE_SINGLE_REGISTER, command, output_value=output)
+    time.sleep(0.1)
+
+def commandReception(command):
+    retry_limit = 10
+    for attempt in range(retry_limit):
+        try:
+            res = e5cc_h.execute(Address, cst.READ_HOLDING_REGISTERS, command, 1)
+            if res is not None:
+                return res
+            else:
+                print(f"received invalid response: {res}")
+        except ModbusInvalidResponseError as e:
+            print(f"ModbusInvalidResponseError: {e}. retrying {attempt + 1}/{retry_limit}...")
+        except ModbusError as e:
+             print(f"ModbusError: {e}. retrying {attempt + 1}/{retry_limit}...")
+        except serial.SerialException as e:
+            print(f"Error: {e}. Retrying {attempt + 1}/{retry_limit}...")
+        time.sleep(1)
+    print("Failed to receive command after retries.")
+    return None
 
 def resultDefault():
     return '\n-'
@@ -31,45 +58,98 @@ def resultDefault():
 def resultERROR():
     return 'ERROR'
 
-# ModBus通信設定
-PORT = '/dev/ttyUSB0'
-e5cc_h = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=2))
-e5cc_h.set_timeout(20)
-Address = 1 # ユニット番号指定
-
-# 書き込み許可(初期設定以外の時はコメントアウト推奨)
-# 動作指令の書き込み変数アドレスは0x0000
-# e5cc_h.execute(Address, cst.WRITE_SINGLE_REGISTER, 0x0000, output_value=0x0001)
-
-def commandInput(command, output):  # コマンド送信
-    # e5cc_h.execute(ユニットNo, 書き込み指定, 書き込む変数のアドレス, output_value=書き込む値（16進数）)
-    e5cc_h.execute(Address, cst.WRITE_SINGLE_REGISTER, command, output_value=output)
-    time.sleep(0.1)
-
-
-def commandReception(command):  # 受信
-    # e5cc_h.execute(ユニットNo, 読み取り指定, 読み取る変数のアドレス, 変数の個数)
-    res = e5cc_h.execute(Address, cst.READ_HOLDING_REGISTERS, command, 1)
-    return res
-
 
 # 現在温度読み取り
 if args.get:
     A1 = commandReception(0x2000)
-    print(A1[0] / 10) # 小数点の分10で割って調整
+    if A1:
+        print(A1[0] / 10)  # 小数点の分10で割って調整
+    else:
+        print('error')
 
-# 恒温槽off（室温２５℃へ戻す）
 if args.off:
     temp_reset = 250
     temp_reset16 = hex(temp_reset)
-    temp_reset16_int = int(temp_reset16)
+    temp_reset16_int = int(temp_reset16, 16)
+    #print(temp_reset16_int)
+    #print(type(temp_reset16_int))
     commandInput(0x2103, temp_reset16_int)
-    A1 = commandReception(0x2103)
+    A1 = commandReception(0x2013)
     if A1:
         print(resultDefault())
     else:
         print(resultERROR())
 
+# 目標温度書き込み＋温度定常待ち
+if args.setandsstate:
+    t_input = float(args.setandsstate[0])
+    t_10 = t_input * 10 # 小数点がなくなるようにずらす
+    t_10_int = int(round(t_10))
+    temp_int = int(hex(t_10_int), 16)
+
+    commandInput(0x2103, temp_int)
+    A1 = commandReception(0x2103)
+    
+    if A1 is None:
+        print(resultERROR())
+    else:
+        tempErr = 0.5 # 温度の許容範囲
+        sleepTime = 5.0 # 判定の間隔 [s]
+        #startTime = time.time()
+
+        while True:
+            try:
+                #if A2 is None:
+                    #print("Command reception failed. Restarting steady state wait...")
+                    #time.sleep(1)
+                    #continue
+                
+                nowTemp = 10000
+                while abs(nowTemp - t_input) >= 0.11:
+                    currentTime = time.time()
+                    A2 = commandReception(0x2000)
+                    if A2 is None:
+                        print("Command reception failed. Restarting steady state wait...")
+                        time.sleep(1)
+                        continue
+                    nowTemp = A2[0] / 10
+                    print(f'nowTemp: {nowTemp} waiting')
+                    codeTime = time.time()
+                    timeDiff = codeTime - currentTime
+                    time.sleep(sleepTime - timeDiff)
+                print('start judgement')
+                count = 0
+                while count <= 10:
+                    currentTime = time.time()
+                    A2 = commandReception(0x2000)
+                    if A2 is None:
+                        print("Command reception failed. Restarting steady state wait...")
+                        time.sleep(1)
+                        break
+                    
+                    nowTemp = A2[0] / 10
+                    if abs(nowTemp - t_input) <= tempErr:
+                        count += 1
+                        print(f'nowTemp: {nowTemp}, count: {count}')
+                        if count >= 10:
+                            print('success')
+                            break
+                        codeTime = time.time()
+                        timeDiff = codeTime - currentTime
+                        time.sleep(sleepTime - timeDiff)
+                    else:
+                        count = 0
+                        print(f'nowTemp: {nowTemp} again')
+                        codeTime = time.time()
+                        timeDiff = codeTime - currentTime
+                        time.sleep(sleepTime - timeDiff)
+
+                if count >= 10:
+                    break
+
+            except Exception as e:
+                print(f"Unexpected error: {e}. Restarting steady state wait...")
+                time.sleep(1)
 
 # 目標温度書き込み
 if args.setrun:
@@ -86,43 +166,7 @@ if args.setrun:
         print(resultDefault())
     else:
         print(resultERROR())
-
-# 目標温度書き込み＋温度定常待ち
-if args.setandsstate:
-    # 温度を16進数に変換
-    t_input = float(args.setrun[0])
-    t_10 = t_input * 10 # 小数点がなくなるようにずらす（20.5℃なら205へ）
-    t_10_int = int(round(t_10)) # 桁落ちしないようにround追加、確認はまだ
-    temp = hex(t_10_int)
-    setTemp = int(temp, 16)
-    # 目標温度書き込み
-    commandInput(0x2103, setTemp)
-    A1 = commandReception(0x2103)
-
-    # エラー処理
-    if A1 is NULL:
-        print(resultERROR())
-        break
-
-    tempErr = 0.5 # 温度の許容範囲[℃]を設定、これは第二引数にするべき？
-    count = 0
-    sleepTime = 5 # 判定の間隔 [s]
-
-    # 定常待ちの処理
-    while count <= 10: # とりあえず10回判定
-        A2 = commandReception(0x2000)
-        nowTemp = A2[0] / 10
-        if abs(nowTemp - setTemp) <= tempErr:
-            count += 1
-            if count >= 10:
-                print('success')
-                break
-            time.sleep(sleepTime)
-        else:
-            count = 0
-            time.sleep(sleepTime)
-
-
+        
 # 定常待ちで使うアクション
 if args.set:
     # 温度を16進数に変換
